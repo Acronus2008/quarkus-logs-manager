aac9e3c67f64fe0735c5e1b7c7d0e9142f55ec8d
Alejandro <alejandro.pantaleon@externos-cl.cencosud.com>
2024-12-02 14:28:50 -0300

    improve log criteria implementation

--- quarkus-logs-manager/src/main/java/com/microboxlabs/service/datasource/criteria/LogCriteria.java
+++ quarkus-logs-manager/src/main/java/com/microboxlabs/service/datasource/criteria/LogCriteria.java
@@ -4,34 +4,36 @@ import com.microboxlabs.service.datasource.domain.Log;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.panache.common.Parameters;
+import io.quarkus.panache.common.Sort;
 import io.smallrye.mutiny.tuples.Tuple2;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class LogCriteria extends AdvanceCriteria<PanacheEntityBase> {
+    private String predicates = "from Log l where 1=1";
+    private final Sort timestamp = Sort.by("timestamp").descending();
 
     @Override
     public PanacheQuery<PanacheEntityBase> query() {
         Tuple2<String, Parameters> objects = prepareQuery(super.getFields());
-        return Log
-                .find(objects.getItem1(), objects.getItem2())
-                .page(super.getPage(), super.getSize());
+        var projection = ((Objects.isNull(super.getFields()) || super.getFields().isEmpty()) && (Objects.nonNull(super.getSearch()) || !super.getSearch().isEmpty()))
+                ? this.searchByDefaultText()
+                : this.findFromFields();
+        return projection.page(super.getPage(), super.getSize());
     }
 
-    private static String createPredicates(Map<String, Object> fields) {
-        var predicates = "from Log l where 1=1";
-        if (fields.containsKey("logLevel"))
-            predicates += " and l.logLevel = :logLevel";
-        if (fields.containsKey("serviceName"))
-            predicates += " and l.serviceName = :serviceName";
-        if (fields.containsKey("startDate"))
-            predicates += " and l.timestamp >= :startDate";
-        if (fields.containsKey("endDate"))
-            predicates += " and l.timestamp <= :endDate";
-        return predicates;
+    private PanacheQuery<PanacheEntityBase> findFromFields() {
+        Tuple2<String, Parameters> objects = prepareQuery(super.getFields());
+        return Log.find(objects.getItem1(), timestamp, objects.getItem2());
     }
 
-    private static Tuple2<String, Parameters> prepareQuery(Map<String, Object> fields) {
+    private PanacheQuery<PanacheEntityBase> searchByDefaultText() {
+        predicates += " or l.logLevel = :logLevel or l.serviceName = :serviceName";
+        return Log.find(predicates, timestamp, Map.of("logLevel", super.getSearch(), "serviceName", super.getSearch()));
+    }
+
+    private Tuple2<String, Parameters> prepareQuery(Map<String, Object> fields) {
         var parameters = new Parameters();
         final var params = fields
                 .entrySet()
@@ -41,5 +43,18 @@ public class LogCriteria extends AdvanceCriteria<PanacheEntityBase> {
                 .orElse(Parameters.with("serviceName", ""));
         final var predicates = createPredicates(fields);
         return Tuple2.of(predicates, params);
+    }
+
+
+    private String createPredicates(Map<String, Object> fields) {
+        if (fields.containsKey("logLevel"))
+            predicates += " and l.logLevel = :logLevel";
+        if (fields.containsKey("serviceName"))
+            predicates += " and l.serviceName = :serviceName";
+        if (fields.containsKey("startDate"))
+            predicates += " and l.timestamp >= :startDate";
+        if (fields.containsKey("endDate"))
+            predicates += " and l.timestamp <= :endDate";
+        return predicates;
     }
 }
